17bb59b6ee99849d6800fe36a05d332898e91fc7
OneConnect_Nkos <OneConnect_Nkos>
2016-08-03 22:14:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oneconnect_nkosi/hellogithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.oneconnect_nkosi.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
